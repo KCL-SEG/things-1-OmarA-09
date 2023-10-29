ecf7c33a00ce73d3a792e6790b1b167863b77903
Omar Ahmad <omar.1.ahmad@kcl.ac.uk>
2023-10-29 14:00:43 +0000

    Make name unique

--- things/models.py
+++ things/models.py
@@ -6,7 +6,7 @@ from django.db.models import Model
 
 # Create your models here.
 class Thing(Model):
-    name = models.CharField(max_length=30,blank=False)
+    name = models.CharField(max_length=30,blank=False, unique=True)
     description = models.CharField(max_length=120,blank=True)
     quantity = models.PositiveIntegerField(
         default=0,
